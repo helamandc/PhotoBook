a626dc89c4aee0095b717e32196f770516c27802
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-02-21 23:43:22 +1100

    Add project files.

---
+++ PhotoBook/PhotoBook.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoBook
+{
+    public class PhotoBook
+    {
+        private int numPages;
+
+        public int GetNumberPages()
+        {
+            return numPages;
+        }
+
+    }
+}
